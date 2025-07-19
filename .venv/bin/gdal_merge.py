19f9f3b5a5fad11589fb6aefd474b95f79e08e6d
Phillip Burrows <48300062+PhillipBurrows@users.noreply.github.com>
2025-07-18 23:27:44 -0600

    Git ignore

---
+++ .venv/bin/gdal_merge.py
@@ -0,0 +1,12 @@
+#!/Users/phillipburrows/verimap/IRmapper-v0-python/.venv/bin/python3
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.gdal_merge as a convenience to use as a script
+from osgeo_utils.gdal_merge import *  # noqa
+from osgeo_utils.gdal_merge import main
+
+deprecation_warn("gdal_merge")
+sys.exit(main(sys.argv))
